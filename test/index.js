2b00fdc68e3d85a1fb2ce121a1a758ddddb968ed
weilee <383523223@qq.com>
2020-05-12 23:09:51 +0800

    使用disableCache替代noCache作为禁用缓存的关键字

--- test/index.js
+++ test/index.js
@@ -42,5 +42,5 @@ proxyMock({
   },
   setSystemProxy: true, // 是否设置系统代理, 默认值 false
   logLevel: 'info', // 日志级别, 默认值级别 info
-  noCache: true // 禁用缓存, 默认值 true
+  disableCache: true // 禁用缓存, 默认值 true
 });
